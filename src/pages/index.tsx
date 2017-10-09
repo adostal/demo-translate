828f2ed5d359954c0b02ef78090fd62fc7fd24ba
Dostal Ales <Ales.Dostal@airbank.cz>
2017-10-09 19:24:44 +0200

    Add Typescript Add React Add Next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+class Index extends React.Component<{}> {
+
+    render() {
+        return (
+            <div>Hello world</div>
+        )
+    }
+}
+
+export default Index;
